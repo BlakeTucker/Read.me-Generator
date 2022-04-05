c32a3e54c60dd055d359b9a982580cf9bbc01974
BlakeTucker <blaketucker12@yahoo.com>
2022-04-05 10:40:32 -0500

    index.js changes

--- utilities/index.js
+++ utilities/index.js
@@ -14,6 +14,11 @@ const questions = [
         message: 'Describe your project.'
        },
        {
+        type: 'input',
+        name: 'github',
+        message: 'Github username'
+       },
+       {
         type: 'list',
         name: 'licensing',
         message: 'What license/lisences will your project need?'
@@ -23,4 +28,25 @@ const questions = [
         name: 'Usage',
         message: 'Explain anything your user should know before/while using your project.'
        },
+       {
+        type: 'input',
+        name: 'Test',
+        message: 'Tests:'
+       },
+       {
+        type: 'input',
+        name: 'email',
+        message: 'Add your email address.'
+       },
+       {
+        type: 'input',
+        name: 'contribute',
+        message: 'What did you contribute, if you did so.'
+       },
+       {
+        type: 'input',
+        name: 'install',
+        message: 'How to install your project.'
+       },
+
 ]
